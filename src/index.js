ea4bb3734da7f4180491444cfc2781b86dcb1b38
AnatoliHaralchuk <sshades@mail.ru>
2021-03-11 22:46:32 +0300

    feat: task decide

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
 exports.min = function min (array) {
-  return array.isArray(array)? Math.min(...array):0
+  return Array.isArray(array)&&array.length>0? Math.min(...array):0
 }
 
 exports.max = function max (array) {
-  return array.isArray(array)? Math.max(...array):0
+  return Array.isArray(array)&&array.length>0? Math.max(...array):0
 }
 
 exports.avg = function avg (array) {
-  return reduce((midlle,item)=>{midlle += item},0)/array.length()
+  return Array.isArray(array)&&array.length>0?array.reduce((midlle,item)=>midlle + item)/array.length:0
+
 }
