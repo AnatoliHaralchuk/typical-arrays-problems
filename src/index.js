6b0f3cf77b9db3ba08e6fc4a24be32d239c9ff74
AnatoliHaralchuk <sshades@mail.ru>
2021-03-11 16:05:26 +0300

    init: start and decide task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array.isArray(array)? Math.min(...array):0
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array.isArray(array)? Math.max(...array):0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return reduce((midlle,item)=>{midlle += item},0)/array.length()
 }
